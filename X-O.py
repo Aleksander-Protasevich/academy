e92cd2cc34a8431ff41c8199eb4f291ec1a8eef0
Aleksander Protasevich <aprotasevitch@gmail.com>
2020-12-27 21:34:41 +0300

    bad work

--- X-O.py
+++ X-O.py
@@ -1,33 +1,35 @@
+import os
 board = """
     1 | 2 | 3  
     ---------- 
     4 | 5 | 6 
     ----------
     7 | 8 | 9 """
-running = True
-while running: 
 step = input('Игрок 1, введите Х или O на выбор: ').upper()
-cell = input('Игрок 1, введите номер ячейки для хода: ')
-dict = {cell: step}
-for key in dict.keys():
-    board = board.replace(key, str(dict[key]))
-if cell != 'X' and cell != 'O':
-    print(board)
-else:
-    print('Эта ячейка занята')
+running = True
+while running:
     cell = input('Игрок 1, введите номер ячейки для хода: ')
-t = board.split()
-if ((t[0] and t[2] and t[4]) == ('X' or 'O')) \
-    or ((t[6] and t[8] and t[10]) == ('X' or 'O')) \
-    or ((t[12] and t[14] and t[16]) == ('X' or 'O')) \
-    or ((t[0] and t[6] and t[12]) == ('X' or 'O')) \
-    or ((t[2] and t[8] and t[14]) == ('X' or 'O')) \
-    or ((t[4] and t[10] and t[16]) == ('X' or 'O')) \
-    or ((t[0] and t[8] and t[16]) == ('X' or 'O')) \
-    or ((t[4] and t[8] and t[12]) == ('X' or 'O')):
-    print('Игрок 1 одержал победу')
-elif (t[0] and t[2] and t[4] and t[6] and t[8] and t[10]
-and t[12] and t[14] and t[16]) == ('X' and 'O'):
-    print('Победила дружба')
-else:
-    print('ура')
+    dict1={'1':'1', '2':'2', '3':'3', '4':'4', 
+    '5':'5', '6':'6', '7':'7', '8':'8', '9':'9'}
+    if dict1[cell] != 'X' and dict1[cell] != 'O':
+        dict1[cell]=step
+        print("{} | {} | {}".format(dict1.get('1'), dict1.get('2'), dict1.get('3')),'\n'
+              "---------", '\n'
+              "{} | {} | {}".format(dict1.get('4'), dict1.get('5'), dict1.get('6')),'\n'
+              "---------", '\n'
+              "{} | {} | {}".format(dict1.get('7'), dict1.get('8'), dict1.get('9')))  
+    else:
+        print('Эта ячейка занята')
+else:                 
+    if ((dict1['1'] == dict1['2'] == dict1['3']) \
+    or (dict1['4'] == dict1['5'] == dict1['5']) \
+    or (dict1['7'] == dict1['8'] == dict1['9']) \
+    or (dict1['1'] == dict1['4'] == dict1['7']) \
+    or (dict1['2'] == dict1['5'] == dict1['8']) \
+    or (dict1['3'] == dict1['6'] == dict1['9']) \
+    or (dict1['1'] == dict1['5'] == dict1['9']) \
+    or (dict1['3'] == dict1['5.'] == dict1['7'])): 
+        print('Игрок 1 одержал победу') 
+    elif (dict1['1'] and dict1['2'] and dict1['3'] and dict1['4'] and dict1['5'] \
+    and dict1['6'] and dict1['7'] and dict1['8'] and dict1['9']) == ('X' or 'O'):
+        print('Победила дружба')
