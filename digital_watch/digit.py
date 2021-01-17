3d6e5b87e263064309b152b2e5d9ef5805b93df3
Aleksander Protasevich <aprotasevitch@gmail.com>
2021-01-17 15:23:29 +0300

    done

--- digital_watch/digit.py
+++ digital_watch/digit.py
@@ -1,71 +1,65 @@
 b = "\u2588"
+p = ' '
 
-zero1 = b*8 
-zero2 = b*2 + '    ' + b*2
-zero3 = b*2 + '    ' + b*2
-zero4 = b*2 + '    ' + b*2
-zero5 = b*8
+zero1 = b*8 + p
+zero2 = b*2 + p*4 + b*2 + p
+zero3 = b*2 + p*4 + b*2 + p
+zero4 = b*2 + p*4 + b*2 + p
+zero5 = b*8 + p
 
-one1 = ' ' + b*4 + '   '
-one2 = '   ' + b*2 + '   ' 
-one3 = '   ' + b*2 + '   ' 
-one4 = '   ' + b*2 + '   ' 
-one5 = b*8
+one1 = p + b*4 + p*4
+one2 = p*3 + b*2 + p*4 
+one3 = p*3 + b*2 + p*4 
+one4 = p*3 + b*2 + p*4 
+one5 = b*8 + p
 
-two1 = b*8
-two2 = '      ' + b*2
-two3 = b*8
-two4 = b*2 + '      '
-two5 = b*8
+two1 = b*8 + p
+two2 = p*6 + b*2 + p
+two3 = b*8 + p
+two4 = b*2 + p*7
+two5 = b*8 + p
 
-three1 = b*8
-three2 = '      ' + b*2
-three3 = b*8
-three4 = '      ' + b*2
-three5 = b*8
+three1 = b*8 + p
+three2 = p*6 + b*2 + p
+three3 = b*8 + p
+three4 = p*6 + b*2 + p
+three5 = b*8 + p
 
-four1 = b*2 + '    ' + b*2
-four2 = b*2 + '    ' + b*2
-four3 = b*8
-four4 = '      ' + b*2
-four5 = '      ' + b*2
+four1 = b*2 + p*4 + b*2 + p
+four2 = b*2 + p*4 + b*2 + p
+four3 = b*8 + p
+four4 = p*6  + b*2 + p
+four5 = p*6  + b*2 + p
 
-five1 = b*8
-five2 = b*2 + '      '
-five3 = b*8
-five4 = '      ' + b*2
-five5 = b*8
+five1 = b*8 + p
+five2 = b*2 + p*7
+five3 = b*8 + p
+five4 = p*6 + b*2 + p
+five5 = b*8 + p
 
-six1 = b*8
-six2 = b*2 + '      '
-six3 = b*8
-six4 = b*2 + '    ' + b*2
-six5 = b*8
+six1 = b*8 + p
+six2 = b*2 + p*7
+six3 = b*8 + p
+six4 = b*2 + p*4 + b*2 + p
+six5 = b*8 + p
 
-seven1 = b*8
-seven2 = '      ' + b*2
-seven3 = '      ' + b*2
-seven4 = '    ' + b*2 + '  '
-seven5 = '  ' + b*2 + '    '
+seven1 = b*8 + p
+seven2 = p*6 + b*2 + p
+seven3 = p*6 + b*2 + p
+seven4 = p*4 + b*2 + p*3
+seven5 = p*2 + b*2 + p*5
 
-eight1 = b*8 
-eight2 = b*2 + '    ' + b*2
-eight3 = b*8
-eight4 = b*2 + '    ' + b*2
-eight5 = b*8
+eight1 = b*8 + p
+eight2 = b*2 + p*4 + b*2 + p
+eight3 = b*8 + p
+eight4 = b*2 + p*4 + b*2 + p
+eight5 = b*8 + p
 
-nine1 = b*8
-nine2 = b*2 + '    ' + b*2
-nine3 = b*8
-nine4 = '      ' + b*2
-nine5 = b*8
-
-
-# print(zero1, ' ', one1, ' ', two1, ' ', three1, ' ', four1, ' ', five1, ' ', six1, ' ', seven1, ' ', eight1, ' ', nine1)
-# print(zero2, ' ', one2, ' ', two2, ' ', three2, ' ', four2, ' ', five2, ' ', six2, ' ', seven2, ' ', eight2, ' ', nine2)
-# print(zero3, ' ', one3, ' ', two3, ' ', three3, ' ', four3, ' ', five3, ' ', six3, ' ', seven3, ' ', eight3, ' ', nine3)
-# print(zero4, ' ', one4, ' ', two4, ' ', three4, ' ', four4, ' ', five4, ' ', six4, ' ', seven4, ' ', eight4, ' ', nine4)
-# print(zero5, ' ', one5, ' ', two5, ' ', three5, ' ', four5, ' ', five5, ' ', six5, ' ', seven5, ' ', eight5, ' ', nine5)
+nine1 = b*8 + p
+nine2 = b*2 + p*4 + b*2 + p
+nine3 = b*8 + p
+nine4 = p*6 + b*2 + p
+nine5 = b*8 + p
 
 if __name__ == "__main__":
     pass
--- digital_watch/main.py
+++ digital_watch/main.py
@@ -1,44 +1,61 @@
 import os
 import datetime
 import time
-from digit import zero1, zero2, zero3, zero4, zero5, one1, one2, one3, one4, one5, two1, two2, two3, two4, two5, three1, three2, three3, three4, three5
-from digit import four1, four2, four3, four4, four5, five1, five2, five3, five4, five5, six1, six2, six3, six4, six5
-from digit import seven1, seven2, seven3, seven4, seven5, eight1, eight2, eight3, eight4, eight5, nine1, nine2, nine3, nine4, nine5
+from digit import *
 
 while True:
     os.system('cls' if os.name == 'nt' else 'clear')
     ct = datetime.datetime.now()
-    str_ct1 = ct.strftime('%H  %M  %S')
-    str_ct2 = ct.strftime('%H  %M  %S')
-    str_ct3 = ct.strftime('%H  %M  %S')
-    str_ct4 = ct.strftime('%H  %M  %S')
-    str_ct5 = ct.strftime('%H  %M  %S')
+    str1 = ct.strftime('%H     %M     %S')
+    str2 = ct.strftime('%H ::  %M ::  %S')
+    str3 = ct.strftime('%H     %M     %S')
+    str4 = ct.strftime('%H ::  %M ::  %S')
+    str5 = ct.strftime('%H     %M     %S')
     
-    line1 = {'0' : zero1, '1': one1, '2' : two1, '3' : three1, '4' : four1, '5' : five1, '6' : six1, '7' : seven1, '8' : eight1, '9' : nine1}
+    line1 = {'0' : zero1, '1': one1, '2' : two1, '3' : three1, '4' : four1, '5' : five1,
+     '6' : six1, '7' : seven1, '8' : eight1, '9' : nine1}
     for x, y in line1.items():
-        str_ct1 = str_ct1.replace(x, y) 
-    # print("{} {} {} {} {} {}".format((str_ct1[0]), (str_ct1[1]), (str_ct1[3]), (str_ct1[4]), (str_ct1[6]), (str_ct1[7]))
-    print(str_ct1)  
+        str1 = str1.replace(x, y) 
+    print(str1)  
 
-    line2 = {'0' : zero2, '1': one2, '2' : two2, '3' : three2, '4' : four2, '5' : five2, '6' : six2, '7' : seven2, '8' : eight2, '9' : nine2}
-    for x, y in line2.items():
-        str_ct2 = str_ct2.replace(x, y) 
-    print(str_ct2)  
-
-    line3 = {'0' : zero3, '1': one3, '2' : two3, '3' : three3, '4' : four3, '5' : five3, '6' : six3, '7' : seven3, '8' : eight3, '9' : nine3}
+    line2 = {'0' : zero2, '1': one2, '2' : two2, '3' : three2, '4' : four2, '5' : five2,
+     '6' : six2, '7' : seven2, '8' : eight2, '9' : nine2, ':' : ' '} 
+    line22 = {'0' : zero2, '1': one2, '2' : two2, '3' : three2, '4' : four2, '5' : five2,
+     '6' : six2, '7' : seven2, '8' : eight2, '9' : nine2, ':' : b}
+    if str2[15] == '0' or str2[15] == '2' or str2[15] == '4' or str2[15] == '6' or str2[15] == '8':
+        for x, y in line2.items():
+            str2 = str2.replace(x, y)
+        print(str2) 
+    else:
+        for x, y in line22.items():
+            str2 = str2.replace(x, y)
+        print(str2)  
+  
+    line3 = {'0' : zero3, '1': one3, '2' : two3, '3' : three3, '4' : four3, '5' : five3,
+     '6' : six3, '7' : seven3, '8' : eight3, '9' : nine3}
     for x, y in line3.items():
-        str_ct3 = str_ct3.replace(x, y)  
-    print(str_ct3)    
+        str3 = str3.replace(x, y)  
+    print(str3)    
     
-    line4 = {'0' : zero4, '1': one4, '2' : two4, '3' : three4, '4' : four4, '5' : five4, '6' : six4, '7' : seven4, '8' : eight4, '9' : nine4}
-    for x, y in line4.items():
-        str_ct4 = str_ct4.replace(x, y) 
-    print(str_ct4)
+    line4 = {'0' : zero4, '1': one4, '2' : two4, '3' : three4, '4' : four4, '5' : five4,
+     '6' : six4, '7' : seven4, '8' : eight4, '9' : nine4, ':' : ' '}
+    line44 = {'0' : zero4, '1': one4, '2' : two4, '3' : three4, '4' : four4, '5' : five4,
+     '6' : six4, '7' : seven4, '8' : eight4, '9' : nine4, ':' : b}
+    if str4[15] == '0' or str4[15] == '2' or str4[15] == '4' or str4[15] == '6' or str4[15] == '8':
+        for x, y in line4.items():
+            str4 = str4.replace(x, y) 
+        print(str4)
+    else:
+        for x, y in line44.items():
+            str4 = str4.replace(x, y) 
+        print(str4)
 
-    line5 = {'0' : zero5, '1': one5, '2' : two5, '3' : three5, '4' : four5, '5' : five5, '6' : six5, '7' : seven5, '8' : eight5, '9' : nine5}
+    line5 = {'0' : zero5, '1': one5, '2' : two5, '3' : three5, '4' : four5, '5' : five5,
+     '6' : six5, '7' : seven5, '8' : eight5, '9' : nine5}
     for x, y in line5.items():
-        str_ct5 = str_ct5.replace(x, y) 
-    print(str_ct5)
-    # print("{} {} {} {} {} {}".format((str_ct1[0]), (str_ct1[1]), (str_ct1[3]), (str_ct1[4]), (str_ct1[6]), (str_ct1[7]))
-    
-    time.sleep(1) 
+        str5 = str5.replace(x, y) 
+    print(str5)
+        
+    time.sleep(1)
+
+
